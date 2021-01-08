9c63279b001adbcf60b80bb853c95fd4ada8bb39
RsolisF <solisfuentesraul@gmail.com>
2021-01-08 10:33:35 +0000

    prueba con filter, reduce y mao con varios lambdas en una misma función.

--- mapFilterReduce.py
+++ mapFilterReduce.py
@@ -7,3 +7,6 @@ listaPares = filter(lambda x: x%2==0, lista)
 listaSumatodo= reduce(lambda x, y: x+y, range(101))
 
 print('{}\n{}\n{}\n'.format(list(listaDoble), list(listaPares), listaSumatodo))
+
+
+sumaPares = reduce(lambda x, y: x+y, filter(lambda x: x%2==0,range(101)))#prueba para usar dos lambda en una misma función reduce y filter
