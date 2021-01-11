6e932f03924691fd320d5d4323ea590e115e7773
RsolisF <solisfuentesraul@gmail.com>
2021-01-11 16:19:05 +0000

    Primera prueba de cración de objetos

---
+++ objetoPerro.py
@@ -0,0 +1,11 @@
+class Perro():
+    def __init__(self, raza, edad, peso):#función constructora, nos permite crear instancias
+        self.raza=raza
+        self.edad=edad
+        self.peso=peso
+    def ladra(slef):
+        print('Guau, guau!!')
+'''Donde pone 'self' es donde payhton meterá el nombre que nosotros asignemos
+al perro, en este caso será Titán'''
+
+Titán = Perro('Labrador', 8, 40)
---
+++ objetosTortuga.py
@@ -0,0 +1,4 @@
+import turtle
+
+tortuga = turtle.Turtle()
+
